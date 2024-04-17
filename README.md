4292c12738f41531439bbd7fc23660dde5413463
Bartłomiej Szostak <BartGOKU@onet.pl>
2024-04-17 21:03:51 +0200

    Lets-a-go!

---
+++ README.md
@@ -0,0 +1,28 @@
+# adaverage  
+
+Small program for showing arithmetic average, dominant and median of given array of numbers  
+
+## Quick Start:  
+
+```console
+$ gnatmake ./src/adaverage.adb
+$ ./adaverage <array of numbers>
+```  
+Example Usage:  
+
+```console
+$ ./adaverage 1 2 2 2 5 5 6
+```  
+
+Expected Output:  
+
+```console
+
+Average:
+      3.286
+Dominant:
+      6.000
+Median:
+      2.000
+
+```
---
+++ src/adaverage.adb
@@ -0,0 +1,57 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Command_Line; use Ada.Command_Line;
+with Ada.Float_Text_IO;
+
+procedure adaverage is
+  type Tablica is array (Integer range <>) of Float;
+--  Msg: String (1 .. 100);
+--  Last: Natural;
+  Arr : Tablica (1 .. Argument_Count);
+  Avrg : Float := 0.0;
+  Dom : Float := 0.0;
+  Buf : Float := 0.0;
+  Med : Float := 0.0;
+  package FLT renames Ada.Float_Text_IO;
+--  package CLI renames Ada.Command_Line;
+begin
+  for i in 1 .. Argument_Count loop
+    Arr (i) := Float'Value (Argument (i));
+  end loop;
+
+  for i in 1 .. Argument_Count loop
+    Avrg := Avrg + Arr (i);
+  end loop;
+  Avrg := Avrg / Float(Argument_Count);
+
+  Dom := Arr (1);
+  for i in 1 .. Argument_Count loop
+    if Dom < Arr (i) then
+      Dom := Arr (i);
+    end if;
+  end loop;
+
+  Buf := Float(Argument_Count);
+  Buf := Buf / 2.0;
+  Buf := Float'Rounding(Buf);
+  if (Argument_Count mod 2) = 0 then
+    Med := (Arr (Integer(Buf)) + Arr (Integer(Buf) + 1)) / 2.0;
+  else
+    Med := Arr (Integer(Buf));
+  end if;
+  
+  New_Line(1);
+
+  Put_Line("Average: ");
+  Put("     ");
+  FLT.Put(Item => Avrg, Fore => 2, Aft => 3, Exp => 0);
+  New_Line(1);
+  Put_Line("Dominant: ");
+  Put("     ");
+  FLT.Put(Item => Dom, Fore => 2, Aft => 3, Exp => 0);
+  New_Line(1);
+  Put_Line("Median: ");
+  Put("     ");
+  FLT.Put(Item => Med, Fore => 2, Aft => 3, Exp => 0);
+  New_Line(1);
+  
+end adaverage;
